96dd34b83a2c655469803f6f75ebd4a301608f70
Lucas Souza <lucasp.sdev@gmail.com>
2022-04-01 23:28:38 -0300

    chore: startup app

---
+++ src/MainFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class MainFrame {
+    private JTextField tfProductName;
+    private JTextField tfProductPrice;
+    private JButton btnOk;
+    private JButton btnClear;
+}
